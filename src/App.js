1a553f73ecd4fced11fcda554ad6f716abf58df4
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-09 15:28:43 -0300

    ciclo de vida

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import { Component } from "react";
 
 class App extends Component{
-  constructor(props){
-    super(props);
-    this.state = {//estado
+  state = {//estado
+      counter: 0,
       posts: [// array
         {
           id: 1,
@@ -15,19 +14,41 @@ class App extends Component{
           title: 'O titulo 2',
           body: 'O corpo 2'
         },
-        {
+         {
           id: 3,
           title: 'O titulo 3',
           body: 'O corpo 3'
         }
       ]
     };
-  }
+
+    timeoutUpdate = null;
+
+    componentDidMount(){
+      this.handleTimeout();
+    }
+
+    componentDidUpdate(){
+      this.handleTimeout();
+    }
+
+    componentWillUnmount(){
+      clearTimeout(this.timeoutUpdate);
+    }
+
+    handleTimeout = () =>{
+      const { posts, counter} = this.state;
+      posts[0].title = 'title mudou'
+      this.timeoutUpdate = setTimeout(()=>{
+        this.setState({ posts, counter: counter+1 });
+      }, 1000);
+    }
 
     render(){//renderizar novamente o componente porque o estado mudou 
-      const { posts} = this.state;//pegando o arry
+      const { posts, counter} = this.state;//pegando o arry
       return(
-        <>
+        <div>
+          <h1>{counter}</h1>
           {posts.map(post => (
             <div>
               <h1 key={post.id}>{post.title}</h1>
@@ -35,7 +56,7 @@ class App extends Component{
             </div>
              
           ))}
-        </>
+        </div>
       );
     }
 }
