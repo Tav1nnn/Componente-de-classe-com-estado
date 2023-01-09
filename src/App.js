b961aa9367c27b1e257294dd3c5564b9b034eb6d
Otavio Marques <marquesotavio251204@gmail.com>
2023-01-09 14:48:24 -0300

    estado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.handlePClick = this.handlePClick.bind(this); //o método tem acesso ao this.state
+    
+    this.state = {//estado
+      name: 'Otavio Marques',
+      counter: 0
+    };
+  }
+
+  handlePClick(){
+    this.setState({name:'Junior'}); //quando meu estado mudar a fução render vai ser chamada
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({ counter: counter + 1 });
+  }
+
+  render(){//renderizar novamente o componente porque o estado mudou 
+    const { name, counter } = this.state;
+    return(
+      <>
+        <h1 onClick={this.handlePClick}>{name} {counter}</h1>
+        <a onClick={this.handleAClick} link="">Este Link</a>
+      </>
+    );
+  }
 }
 
 export default App;
